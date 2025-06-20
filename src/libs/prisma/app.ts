565401550af9bdbb80e43168d1403a5a188dd027
[James] Jirapat Sukhonthapong 😊 <jirapat.su@yahoo.com>
2025-06-20 12:09:30 +0700

    ✨ feat(prisma): enhance appDB with performance logging for all operations

--- src/libs/prisma/app.ts
+++ src/libs/prisma/app.ts
@@ -6,7 +6,23 @@ const globalForPrisma = global as unknown as {
   appDB: PrismaClient
 }
 
-const appDB = globalForPrisma.appDB || new PrismaClient().$extends(withAccelerate())
+const appDB = globalForPrisma.appDB || new PrismaClient().$extends(withAccelerate()).$extends({
+  query: {
+    $allModels: {
+      async $allOperations({ args, model, operation, query }) {
+        const start = performance.now()
+        const result = await query(args)
+        const end = performance.now()
+        const time = end - start
+        // eslint-disable-next-line no-console
+        console.log(
+          `[APP_DB] Operation: ${operation} - Model: ${model} - Time: ${time.toFixed(2)}ms - Args: ${JSON.stringify(args)}`,
+        )
+        return result
+      },
+    },
+  },
+})
 
 if (process.env.NODE_ENV !== 'production')
   globalForPrisma.appDB = appDB
