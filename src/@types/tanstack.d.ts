ee57e790567fe035d568d7791160617a43458e43
[James] Jirapat Sukhonthapong 😊 <jirapat.su@yahoo.com>
2025-07-16 03:09:00 +0700

    :sparkles: feat: replace defaultError type with TreatyError in react-query module and add TreatyError type definition

--- src/@types/tanstack.d.ts
+++ src/@types/tanstack.d.ts
@@ -1,21 +1,8 @@
 /* eslint-disable ts/consistent-type-definitions */
-import type { HTTP_STATUS } from '@/src/constants/http-status'
-
-import type { ErrorService } from '../utils/error-service'
-
-type APIError = {
-  status: number
-  value: {
-    detail?: ErrorService
-    message: string
-    'request-id': string
-    status: keyof typeof HTTP_STATUS
-    success: boolean
-  }
-}
+import type { TreatyError } from '@/src/libs/eden/utils'
 
 declare module '@tanstack/react-query' {
   interface Register {
-    defaultError: APIError
+    defaultError: TreatyError
   }
 }
